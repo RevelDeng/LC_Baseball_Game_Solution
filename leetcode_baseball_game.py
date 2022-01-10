d89aec64ecf90cccb402ac745355ee7709509dfa
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-09 19:53:44 -0500

    Leetcode Baseball Game solution

---
+++ leetcode_baseball_game.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        record = []
+        for i in range(len(ops)):
+            try:
+                record.append(int(ops[i]))
+            except:
+                pass
+            if ops[i] == "+":
+                record.append(sum(record[-2:]))
+            elif ops[i] == "D":
+                record.append(2 * record[-1])
+            elif ops[i] == "C":
+                record.pop()
+        return sum(record)
